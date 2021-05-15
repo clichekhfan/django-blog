4117f2b77a3ab59b2c2b2957ba6c79ae2f9f6525
Andrew Hanson <ahanso2@uw.edu>
2021-05-15 13:00:04 -0700

    added blog view/urls/templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404
+from blog.models import Post
 
-# Create your views here.
+
+def list_view(request):
+    context = {'posts': Post.objects.all()}
+    return render(request, 'blog/list.html', context)
+
+
+def detail_view(request, post_id):
+    try:
+        post = Post.objects.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+
+    if request.method == "POST":
+        raise Http404
+
+    context = {'post': post}
+    return render(request, 'blog/detail.html', context)
