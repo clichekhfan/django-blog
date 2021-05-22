c53e34a9e943b71a4e9b35421933d9f46c6c8c57
Andrew Hanson <ahanso2@uw.edu>
2021-05-22 13:15:57 -0700

    fixed admin bug

--- mysite/blog/admin.py
+++ mysite/blog/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
 from blog.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryAdmin(admin.ModelAdmin):
+    # fields = ('name', 'description', 'posts', )
+    exclude = ('posts', )
+
+
+class CategoryInline(admin.TabularInline):
+    model = Category.posts.through
+
+
+class PostAdmin(admin.ModelAdmin):
+    # fields = ('title', 'text', 'author', 'created_date', 'modified_date', 'published_date', )
+    inlines = [
+        CategoryInline,
+    ]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
