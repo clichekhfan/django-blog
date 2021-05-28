675b4e5f8e71021207b36d7b22fabda3ce6e6abb
Andrew Hanson <ahanso2@uw.edu>
2021-05-28 10:52:33 -0700

    blog class views enabled

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse, HttpResponseRedirect
 from blog.models import Post
-from django.template import loader
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+
+
+class BlogListView(ListView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blog/list.html'
+
+
+class BlogDetailView(DetailView):
+    queryset = Post.objects.exclude(published_date__exact=None).order_by('-published_date')
+    template_name = 'blog/detail.html'
 
 
 def stub_view(request, *args, **kwargs):
@@ -15,18 +26,3 @@ def stub_view(request, *args, **kwargs):
     return HttpResponse(body, content_type="text/plain")
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by("-published_date")
-    context = {'posts': posts}
-    return render(request, 'blog/list.html', context)
-
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blog/detail.html', context)
